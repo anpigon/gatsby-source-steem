bd8b5d4ec545fd349cf287ccfdc66a995df13a56
안피곤(anpigon) <markan82@gmail.com>
2018-12-20 00:31:59 +0900

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,55 @@
+const fs = require('fs');
 const crypto = require('crypto');
 const getAll = require('./get-all');
 
-exports.sourceNodes = async ({ boundActionCreators }, { tag, sortBy }) => {
+exports.sourceNodes = async ({ boundActionCreators }, { path, tag, sortBy }) => {
   const { createNode } = boundActionCreators;
+
+  function createPost(post) {
+    const title = post.title
+    const date = new Date(`${post.created}Z`);
+    const json_metadata = JSON.parse(post.json_metadata);
+    const tags = json_metadata.tags || [];
+    const category = json_metadata.category || (title.match(/(?<=^\[)([a-zA-Zㄱ-힣\s]*)(?=\])/g) || ""); //title.match(/(?<=^\[)([^}]*)(?=\])/g)
+    const content = [
+      '---',
+      `title: "${title}"`,
+      // `author: ${post.author}`,
+      `date: "${post.created}Z"`,
+      `layout: post`,
+      `draft: false`,
+      `path: "${post.url}"`,
+      `category: "${category}"`,
+      `tags:`,
+      ...tags.map(tag => `  - "${tag}"`),
+      `description: "${post.body.replace(/\n/g, ' ').substr(0, 200)}"`,
+      '---',
+      `${post.body}`
+    ]
+    const fileName = `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2,'0')}-${(date.getDate()).toString().padStart(2,'0')}---${post.permlink}`;
+    fs.writeFileSync(`${filePath}/${fileName}.md`, content.join('\n'), 'utf8');
+  }
 
   const posts = await getAll(tag, sortBy);
   posts.map(post => {
-    const nodeStr = JSON.stringify(post);
-    const nodeHash = crypto
-      .createHash('md5')
-      .update(nodeStr)
-      .digest('hex');
+    createPost(post);
+    // const nodeStr = JSON.stringify(post);
+    // const nodeHash = crypto
+    //   .createHash('md5')
+    //   .update(nodeStr)
+    //   .digest('hex');
 
-    createNode({
-      ...post,
-      id: `${post.id}`,
-      parent: null,
-      children: [],
-      internal: {
-        type: 'SteemPost',
-        content: nodeStr,
-        contentDigest: nodeHash,
-      },
-    });
+    // createNode({
+    //   ...post,
+    //   id: `${post.id}`,
+    //   parent: null,
+    //   children: [],
+    //   internal: {
+    //     type: 'SteemPost',
+    //     content: nodeStr,
+    //     contentDigest: nodeHash,
+    //   },
+    // });
   });
 
   return;
